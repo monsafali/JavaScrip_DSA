876b682ec87c8cc44f1d6394733720f85bf59da5
monsafali <monsafali272@gmail.com>
2024-06-25 13:59:03 +0500

    inserting element in array

---
+++ 03_Array_insertying/aap.js
@@ -0,0 +1,36 @@
+// let data = [10,60,98,40,45]
+// let newEl = 70;
+// let position = 2;
+// console.log(data)
+
+
+// for (let i=data.length-1; i>=0; i--){
+//     // console.warn(i)
+//     // console.log(i)
+//     if(i>= position) {
+//         data[i+1] = data[i]
+//         if(i === position){
+//             data[i] = newEl
+//         }
+//     }
+
+// }
+
+
+function inserEl(){
+    let data = [10,60,98,40,45];
+    console.log(data)
+    let newEl =  parseInt(document.getElementById("newEl").value);
+    let position = document.getElementById("position").value;
+
+    for(let i= data.length-1;i>=0; i--){
+    if(i>= position) {
+            data[i+1] = data[i]
+            if(i == position){
+                data[i] = newEl
+            }
+         }
+    }
+    console.log(data)
+
+}
---
+++ 03_Array_insertying/readme.md
@@ -0,0 +1,6 @@
+Inserting Element in Array
+Understang isuse and solution wiht whiteboard
+Make array define the position 
+Use loop to insert tnew element
+Insert element with text input and button
+insert Element with by default funtion.
