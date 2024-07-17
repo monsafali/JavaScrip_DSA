c93199996f2bc65f5de7b47b22323ba332e2b729
monsafali <monsafali272@gmail.com>
2024-07-17 16:10:15 +0500

    indirect recursion in js

---
+++ 10_indirect_Recursion/aap.js
@@ -0,0 +1,28 @@
+   //   let money = 100;
+    //   let totalApple = 0;
+
+    //   function buyApple(x) {
+    //     if (x > 0) {
+    //       console.warn("I have", x, "RS", totalApple);
+    //       buyMore(x - 10);
+    //     } else {
+    //       console.warn("I don't have more money", totalApple);
+    //     }
+    //   }
+    //   function buyMore(x) {
+    //     totalApple++;
+    //     buyApple(x - 5);
+    //   }
+
+    //   buyApple(money);
+    function test(x)
+    {
+     // console.warn(x)
+     if(x>0){
+         test(x-1)
+     }
+     console.warn(x)
+    }
+    
+    let data=5;
+    test(data)
