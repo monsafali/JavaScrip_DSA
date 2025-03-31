147d5226ef819ea33c18eb9523f52c1819736b83
monsafali <monsafali272@gmail.com>
2025-03-31 13:58:47 +0500

    something new added

--- 03_Array_insertying/aap.js
+++ 03_Array_insertying/aap.js
@@ -3,7 +3,6 @@
 // let position = 2;
 // console.log(data)
 
-
 // for (let i=data.length-1; i>=0; i--){
 //     // console.warn(i)
 //     // console.log(i)
@@ -13,24 +12,25 @@
 //             data[i] = newEl
 //         }
 //     }
-
 // }
 
+function inserEl() {
+  let data = [10, 60, 98, 40, 45];
+  console.log(data);
+  let newEl = parseInt(document.getElementById("newEl").value);
+  let position = document.getElementById("position").value;
 
-function inserEl(){
-    let data = [10,60,98,40,45];
-    console.log(data)
-    let newEl =  parseInt(document.getElementById("newEl").value);
-    let position = document.getElementById("position").value;
-
-    for(let i= data.length-1;i>=0; i--){
-    if(i>= position) {
-            data[i+1] = data[i]
-            if(i == position){
-                data[i] = newEl
-            }
-         }
+  for (let i = data.length - 1; i >= 0; i--) {
+    if (i >= position) {
+      data[i + 1] = data[i];
+      if (i == position) {
+        data[i] = newEl;
+      }
     }
-    console.log(data)
-
+  }
+  console.log(data);
 }
+
+// let data = [10, 60, 98, 40, 45];
+// data.splice(2, 0, 800);
+// console.log(data);
